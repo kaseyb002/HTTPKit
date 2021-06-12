10a03f7155c6dede0fb72c3137ea42709b80ad66
Kasey Baughan <kasey@route.com>
2021-06-12 10:30:09 -0600

    more async await support

--- Sources/HTTPKit/HTTPLoadable.swift
+++ Sources/HTTPKit/HTTPLoadable.swift
@@ -5,4 +5,8 @@ public protocol HTTPLoadable {
     @discardableResult
     func send(_ request: HTTPRequestable,
               callback: @escaping (HTTPResult) -> ()) -> Cancellable?
+    
+    @available(macOS 12.0, *)
+    @available(iOS 15.0, *)
+    func send(_ request: HTTPRequestable) async throws -> HTTPResponse
 }
